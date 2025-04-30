a37eada828e556288e064706add47f07cfb6b2dc
elijah <eli.dunbar18@gmail.com>
2025-04-30 07:12:12 -0400

    now we have a player sprite

--- main.py
+++ main.py
@@ -3,20 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    pygame.Surface.fill(screen, (0,0,0))
-    pygame.display.flip()
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		pygame.Surface.fill(screen, (0,0,0))
+		player.draw(screen)
+		pygame.display.flip()
+
+		# limit the framerate to 60 FPS
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
